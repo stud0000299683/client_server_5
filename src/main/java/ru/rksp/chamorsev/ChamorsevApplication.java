f9dd0b459b9bf403c55e0fdbeea0ee5fd4382aa7
chamorcev <stud0000299683@utmn.ru>
2025-10-16 11:11:20 +0300

    first commit Lab1

---
+++ src/main/java/ru/rksp/chamorsev/ChamorsevApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.chamorsev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamorsevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamorsevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/chamorsev/ChamorsevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.chamorsev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamorsevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
